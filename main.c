1c99b3252b8178eb88d366676395c339109bdd9b
Olawoyin <yustee2017@gmail.com>
2023-11-15 20:41:42 +0100

    some changes made

--- main.c
+++ main.c
@@ -1,134 +1,121 @@
-#include "shell.h"
-
-void sig_handler(int sig);
-int execute(char **args, char **front);
-
+#include "main.h"
 /**
- * sig_handler - Prints a new prompt upon a signal.
- * @sig: The signal.
+ * main - initialize the variables of the program
+ * @argc: number of values received from the command line
+ * @argv: values received from the command line
+ * @env: number of values received from the command line
+ * Return: zero on success.
  */
-void sig_handler(int sig)
+int main(int argc, char *argv[], char *env[])
 {
-	char *new_prompt = "\n$ ";
+    shell_data data_struct = {NULL}, *data = &data_struct;
+    char *prompt = "";
 
-	(void)sig;
-	signal(SIGINT, sig_handler);
-	write(STDIN_FILENO, new_prompt, 3);
+    initialize_data(data, argc, argv, env);
+
+    signal(SIGINT, handle_ctrl_c);
+
+    if (isatty(STDIN_FILENO) && isatty(STDOUT_FILENO) && argc == 1)
+    {              /* We are in the terminal, interactive mode */
+        errno = 2; /*???????*/
+        prompt = PROMPT_MSG;
+    }
+    errno = 0;
+    rollover(prompt, data);
+    return (0);
 }
 
 /**
- * execute - Executes a command in a child process.
- * @args: An array of arguments.
- * @front: A double pointer to the beginning of args.
- *
- * Return: If an error occurs - a corresponding error code.
- *         O/w - The exit value of the last executed command.
+ * handle_ctrl_c - print the prompt in a new line
+ * when the signal SIGINT (ctrl + c) is send to the program
+ * @UNUSED: option of the prototype
  */
-int execute(char **args, char **front)
+void handle_ctrl_c(int opr UNUSED)
 {
-	pid_t child_pid;
-	int status, flag = 0, ret = 0;
-	char *command = args[0];
-
-	if (command[0] != '/' && command[0] != '.')
-	{
-		flag = 1;
-		command = get_location(command);
-	}
-
-	if (!command || (access(command, F_OK) == -1))
-	{
-		if (errno == EACCES)
-			ret = (create_error(args, 126));
-		else
-			ret = (create_error(args, 127));
-	}
-	else
-	{
-		child_pid = fork();
-		if (child_pid == -1)
-		{
-			if (flag)
-				free(command);
-			perror("Error child:");
-			return (1);
-		}
-		if (child_pid == 0)
-		{
-			execve(command, args, environ);
-			if (errno == EACCES)
-				ret = (create_error(args, 126));
-			free_env();
-			free_args(args, front);
-			free_alias_list(aliases);
-			_exit(ret);
-		}
-		else
-		{
-			wait(&status);
-			ret = WEXITSTATUS(status);
-		}
-	}
-	if (flag)
-		free(command);
-	return (ret);
+    _print("\n");
+    _print(PROMPT_MSG);
 }
 
 /**
- * main - Runs a simple UNIX command interpreter.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
- *
- * Return: The return value of the last executed command.
+ * initialize_data - initialize the struct with the info of the program
+ * @data: pointer to the structure of data
+ * @argv: array of arguments pased to the program execution
+ * @env: environ pased to the program execution
+ * @argc: number of values received from the command line
  */
-int main(int argc, char *argv[])
+void initialize_data(shell_data *data, int argc, char *argv[], char **env)
 {
-	int ret = 0, retn;
-	int *exe_ret = &retn;
-	char *prompt = "$ ", *new_line = "\n";
+    int i = 0;
 
-	name = argv[0];
-	hist = 1;
-	aliases = NULL;
-	signal(SIGINT, sig_handler);
+    data->program_name = argv[0];
+    data->input_line = NULL;
+    data->command_name = NULL;
+    data->exec_counter = 0;
+    /* define the file descriptor to be readed*/
+    if (argc == 1)
+        data->file_descriptor = STDIN_FILENO;
+    else
+    {
+        data->file_descriptor = open(argv[1], O_RDONLY);
+        if (data->file_descriptor == -1)
+        {
+            _printe(data->program_name);
+            _printe(": 0: Can't open ");
+            _printe(argv[1]);
+            _printe("\n");
+            exit(127);
+        }
+    }
+    data->tokens = NULL;
+    data->env = malloc(sizeof(char *) * 50);
+    if (env)
+    {
+        for (; env[i]; i++)
+        {
+            data->env[i] = str_duplicate(env[i]);
+        }
+    }
+    data->env[i] = NULL;
+    env = data->env;
 
-	*exe_ret = 0;
-	environ = _copyenv();
-	if (!environ)
-		exit(-100);
-
-	if (argc != 1)
-	{
-		ret = proc_file_commands(argv[1], exe_ret);
-		free_env();
-		free_alias_list(aliases);
-		return (*exe_ret);
-	}
-
-	if (!isatty(STDIN_FILENO))
-	{
-		while (ret != END_OF_FILE && ret != EXIT)
-			ret = handle_args(exe_ret);
-		free_env();
-		free_alias_list(aliases);
-		return (*exe_ret);
-	}
-
-	while (1)
-	{
-		write(STDOUT_FILENO, prompt, 2);
-		ret = handle_args(exe_ret);
-		if (ret == END_OF_FILE || ret == EXIT)
-		{
-			if (ret == END_OF_FILE)
-				write(STDOUT_FILENO, new_line, 1);
-			free_env();
-			free_alias_list(aliases);
-			exit(*exe_ret);
-		}
-	}
-
-	free_env();
-	free_alias_list(aliases);
-	return (*exe_ret);
+    data->alias_list = malloc(sizeof(char *) * 20);
+    for (i = 0; i < 20; i++)
+    {
+        data->alias_list[i] = NULL;
+    }
 }
+/**
+ * rollover - its a infinite loop that shows the prompt
+ * @prompt: prompt to be printed
+ * @data: its a infinite loop that shows the prompt
+ */
+void rollover(char *prompt, shell_data *data)
+{
+    int error_code = 0, string_len = 0;
+
+    while (++(data->exec_counter))
+    {
+        _print(prompt);
+        error_code = string_len = _getline(data);
+
+        if (error_code == EOF)
+        {
+            free_all_data(data);
+            exit(errno); /* if EOF is the fisrt Char of string, exit*/
+        }
+        if (string_len >= 1)
+        {
+            expand_alias(data);
+            expand_variables(data);
+            tokenize(data);
+            if (data->tokens[0])
+            { /* if a text is given to prompt, execute */
+                error_code = execute(data);
+                if (error_code != 0)
+                    _print_error(error_code, data);
+            }
+            free_recurrent_data(data);
+        }
+    }
+}
+
---
+++ main.h
@@ -0,0 +1,228 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>  /* for printf*/
+#include <unistd.h> /* for fork, execve*/
+#include <stdlib.h>
+#include <string.h> /* for strtok*/
+#include <stddef.h>
+#include <errno.h>     /* for errno and perror */
+#include <sys/types.h> /* for type pid */
+#include <sys/wait.h>  /* for wait */
+#include <sys/stat.h>  /* for use of stat function */
+#include <signal.h>    /* for signal management */
+#include <fcntl.h>     /* for open files*/
+
+/************* MACROS **************/
+
+#include "macros.h" /* for msg help and prompt */
+
+/************* STRUCTURES **************/
+
+/**
+ * struct info- struct for the program's data
+ * @program_name: the name of the executable
+ * @input_line: pointer to the input read for _getline
+ * @command_name: pointer to the first command typed by the user
+ * @exec_counter: number of excecuted commands
+ * @file_descriptor: file descriptor to the input of commands
+ * @tokens: pointer to array of tokenized input
+ * @env: copy of the environ
+ * @alias_list: array of pointers with aliases.
+ */
+typedef struct info
+{
+    char *program_name;
+    char *input_line;
+    char *command_name;
+    int exec_counter;
+    int file_descriptor;
+    char **tokens;
+    char **env;
+    char **alias_list;
+} shell_data;
+
+/**
+ * struct builtins - struct for the builtins
+ * @builtin: the name of the builtin
+ * @function: the associated function to be called for each builtin
+ */
+typedef struct builtins
+{
+    char *builtin;
+    int (*function)(shell_data *data);
+} builtins;
+
+/************* MAIN FUNCTIONS *************/
+
+/*========  shell.c  ========*/
+
+/* Initialize the struct with the info of the program */
+void initialize_data(shell_data *data, int arc, char *argv[], char **env);
+
+/* Makes the infinite loop that shows the prompt*/
+void rollover(char *prompt, shell_data *data);
+
+/* Print the prompt in a new line */
+void handle_ctrl_c(int opr UNUSED);
+
+/*========  _getline.c  ========*/
+
+/* Read one line of the standar input*/
+int _getline(shell_data *data);
+
+/* split the each line for the logical operators if it exist */
+int check_logic_ops(char *array_commands[], int i, char array_operators[]);
+
+/*======== expansions.c ========*/
+
+/* expand variables */
+void expand_variables(shell_data *data);
+
+/* expand aliases */
+void expand_alias(shell_data *data);
+
+/* append the string to the end of the buffer*/
+int buffer_add(char *buffer, char *str_to_add);
+
+/*======== str_tok.c ========*/
+
+/* Separate the string in tokens using a designed delimiter */
+void tokenize(shell_data *data);
+
+/* Creates a pointer to a part of a string */
+char *_strtok(char *line, char *delim);
+
+/*======== execute.c ========*/
+
+/* Execute a command with its entire path */
+int execute(shell_data *data);
+
+/*======== builtins_list.c ========*/
+
+/* If match a builtin, executes it */
+int builtins_list(shell_data *data);
+
+/*======== find_in_path.c ========*/
+
+/* Creates an array of the path directories */
+char **tokenize_path(shell_data *data);
+
+/* Search for program in path */
+int find_program(shell_data *data);
+
+/************** HELPERS FOR MEMORY MANAGEMENT **************/
+
+/*======== helpers_free.c ========*/
+
+/* Frees the memory for directories */
+void free_array_of_pointers(char **directories);
+
+/* Free the fields needed each loop */
+void free_recurrent_data(shell_data *data);
+
+/* Free all field of the data */
+void free_all_data(shell_data *data);
+
+/************** BUILTINS **************/
+
+/*======== builtins_more.c ========*/
+
+/* Close the shell */
+int builtin_exit(shell_data *data);
+
+/* Change the current directory */
+int builtin_cd(shell_data *data);
+
+/* set the work directory */
+int set_work_directory(shell_data *data, char *new_dir);
+
+/* show help information */
+int builtin_help(shell_data *data);
+
+/* set, unset and show alias */
+int builtin_alias(shell_data *data);
+
+/*======== builtins_env.c ========*/
+
+/* Shows the environment where the shell runs */
+int builtin_env(shell_data *data);
+
+/* create or override a variable of environment */
+int builtin_set_env(shell_data *data);
+
+/* delete a variable of environment */
+int builtin_unset_env(shell_data *data);
+
+/************** HELPERS FOR ENVIRONMENT VARIABLES MANAGEMENT **************/
+
+/*======== env_management.c ========*/
+
+/* Gets the value of an environment variable */
+char *env_get_key(char *name, shell_data *data);
+
+/* Overwrite the value of the environment variable */
+int env_set_key(char *key, char *value, shell_data *data);
+
+/* Remove a key from the environment */
+int env_remove_key(char *key, shell_data *data);
+
+/* prints the current environ */
+void print_environ(shell_data *data);
+
+/************** HELPERS FOR PRINTING **************/
+
+/*======== helpers_print.c ========*/
+
+/* Prints a string in the standar output */
+int _print(char *string);
+
+/* Prints a string in the standar error */
+int _printe(char *string);
+
+/* Prints a string in the standar error */
+int _print_error(int errorcode, shell_data *data);
+
+/************** HELPERS FOR STRINGS MANAGEMENT **************/
+
+/*======== helpers_string.c ========*/
+
+/* Counts the number of characters of a string */
+int str_length(char *string);
+
+/* Duplicates an string */
+char *str_duplicate(char *string);
+
+/* Compares two strings */
+int str_compare(char *string1, char *string2, int number);
+
+/* Concatenates two strings */
+char *str_concat(char *string1, char *string2);
+
+/* Reverse a string */
+void str_reverse(char *string);
+
+/*======== helpers_numbers.c ========*/
+
+/* Cast from int to string */
+void long_to_string(long number, char *string, int base);
+
+/* convert an string in to a number */
+int _atoi(char *s);
+
+/* count the coincidences of character in string */
+int count_characters(char *string, char *character);
+
+/*======== alias_management.c ========*/
+
+/* print the list of alias */
+int print_alias(shell_data *data, char *alias);
+
+/* get the alias name */
+char *get_alias(shell_data *data, char *alias);
+
+/* set the alias name */
+int set_alias(char *alias_string, shell_data *data);
+
+#endif /* SHELL_H */
+
